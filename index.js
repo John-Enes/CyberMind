1a1d012e0e587db6063610e18fed98a9b90c72ba
John-Enes <joaoenesoficial@gmail.com>
2024-04-07 17:53:12 -0300

    subfinalatt

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ document.getElementById("btn-topo").addEventListener("click", function() {
     document.documentElement.scrollTop = 0; // Para outros navegadores
 });
 
-
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 5000});
+ScrollReveal().reveal('.punchline', { delay: 5000 });
